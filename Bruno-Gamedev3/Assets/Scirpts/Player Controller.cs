fa564cf6f355850b494684a0c6e3268b76931e9d
alunosg <alunosg.altodalapa@gmail.com>
2026-03-28 15:31:17 -0300

    Fiz a mira o tiro e coloque os efeitos

--- Bruno-Gamedev3/Assets/Scirpts/Player Controller.cs
+++ Bruno-Gamedev3/Assets/Scirpts/Player Controller.cs
@@ -15,7 +15,32 @@ public class PlayerController : MonoBehaviour
     public float minRotX = -30;
     public float maxRotX = 75;
     public Transform camTarget;
+
+
     private Vector2 moveInput;
+
+    public Transform aimImage;
+    public GameObject bulletPrefab;
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            Rigidbody b = Instantiate(bulletPrefab,
+                camTarget.position + camTarget.forward * 1,
+                camTarget.rotation).GetComponent<Bullet>().rig;
+
+            b.linearVelocity = rig.linearVelocity;
+
+        }
+
+
+
+
+
+
+
+    }
 
 
     public void Move(InputAction.CallbackContext context)
